5380681f8e837dc696264ec0237cd95a6065975d
Guilherme Matos <guimvmatos1@outlook.com>
2021-03-26 21:29:10 -0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -57,7 +57,7 @@ function action_trigger() {
 
     // send an event to the bot, so that bot can start the conversation by greeting the user
     $.ajax({
-        url: `https://finalchatbot.herokuapp.com/conversations/${user_id}/execute`,
+        url: `https://gothan.herokuapp.com/conversations/${user_id}/execute`,
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({ "name": action_name, "policy": "MappingPolicy", "confidence": "0.98" }),
@@ -156,7 +156,7 @@ function scrollToBottomOfResults() {
 function send(message) {
 
     $.ajax({
-        url: "https://finalchatbot.herokuapp.com/webhooks/rest/webhook",
+        url: "https://gothan.herokuapp.com/webhooks/rest/webhook",
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({ message: message, sender: user_id }),
